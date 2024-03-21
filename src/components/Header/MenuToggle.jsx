f1358f70cf204388ab1cce1543168ce1f54f8dcd
Ethan Zang <zang.ethan@gmail.com>
2024-03-21 16:50:30 -0400

    Create header, move unused code to archive folder

--- src/components/Header/MenuToggle.jsx
+++ src/components/Header/MenuToggle.jsx
@@ -1,10 +1,12 @@
-import React from "react"
-import { Box } from "@chakra-ui/react"
+import React from 'react'
+import { Box} from "@chakra-ui/react";
+import CloseIcon from './CloseIcon';
+import MenuIcon from './MenuIcon';
 
-const MenuToggle = ({ toggle, isOpen }) => {
+export default function MenuToggle({ toggle, isOpen }) {
   return (
     <Box display={{ base: "block", md: "none" }} onClick={toggle}>
-      {isOpen ? <CloseIcon /> : <MenuIcon />}
-    </Box>
+    {isOpen ? <CloseIcon /> : <MenuIcon />}
+  </Box>
   )
 }
