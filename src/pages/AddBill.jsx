8f90d537e48058c4abfba4495995c0fc41d2a3b1
Lisa Yan <lisayan@Lisas-MacBook-Pro.local>
2024-03-26 22:18:54 -0400

    Added submission form for submitting bill

--- src/pages/AddBill.jsx
+++ src/pages/AddBill.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import { ChakraProvider, Box, Text } from "@chakra-ui/react";
+import { ChakraProvider, Button, Input, Text } from "@chakra-ui/react";
 import Header from "../components/Header";
+import "../styles/addbill.css"
 
 export default function AddBillPage() {
     return (
         <div>
             <Header />
-            <Box p={4} borderWidth="1px" borderRadius="lg">
-                <Text fontSize="lg" fontWeight="bold">Add Bill Page</Text>
-                {/* Content of the new page */}
-            </Box>
+            <Text fontSize="30px" fontWeight="bold" marginBottom="20px" align="center">Add Your Medical Bill</Text>
+            <form className="addBillForm">
+                <Input className="input-field"
+                    placeholder="Insurance" />
+                <Input className="input-field"
+                    placeholder="City" />
+                <Input className="input-field"
+                    placeholder="State" />
+                <Input className="input-field"
+                    placeholder="Procedure (e.g. MRI)" />
+                <Input className="input-field"
+                    placeholder="Provider" />
+                <Input className="input-field"
+                    placeholder="Amount Paid (Insurance)" />
+                <Input className="input-field"
+                    placeholder="Amount Paid (You)" />
+                <Button className="submitButton"
+                    type="submit">Submit Bill</Button>
+            </form>
         </div>
     )
 };
