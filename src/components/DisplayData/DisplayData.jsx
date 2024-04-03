feba95a46d8b5cecbb17e9ff861d017ef921c9c3
Ethan Zang <zang.ethan@gmail.com>
2024-04-02 21:53:51 -0400

    Tmp commit for ddb_setup

--- src/components/DisplayData/DisplayData.jsx
+++ src/components/DisplayData/DisplayData.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AWS from 'aws-sdk';
 import HomePageTabs from './HomePageTabs/Tabs.jsx';
 import BoxPlotComponent from './SummaryBoxPlots/Boxplots.jsx';
 import { dataObject } from '../../data/data.jsx';
@@ -6,6 +7,55 @@ import PieChart from './SummaryPieCharts/PieCharts.jsx';
 import { boxContainer, parentContainer } from '../../styles/plots.css';
 
 export default function DisplayData() {
+  // DDB testing
+  useEffect(() => {
+    // Configure AWS SDK with your region
+    AWS.config.update({ region: 'us-east-2' });
+    AWS.config.update({
+      accessKeyId: 'AKIA6ODUZTKWCTGNDVLT',
+      secretAccessKey: '',
+      region: 'us-east-2'
+    });
+
+    // Create an STS client
+    const sts = new AWS.STS();
+
+    // Parameters for assuming the IAM role
+    const params = {
+      RoleArn: 'arn:aws:iam::992382393004:role/BillsExposedDynamoDBAccess',
+      RoleSessionName: 'Session0', // Provide a session name
+    };
+
+    // Assume the IAM role
+    sts.assumeRole(params, (err, data) => {
+      if (err) {
+        console.error('Error assuming IAM role:', err);
+        return;
+      }
+    })
+
+    //   // Use the temporary credentials to make authenticated requests
+    //   const credentials = {
+    //     accessKeyId: data.Credentials.AccessKeyId,
+    //     secretAccessKey: data.Credentials.SecretAccessKey,
+    //     sessionToken: data.Credentials.SessionToken,
+    //   };
+
+    //   // Now you can use these temporary credentials to access AWS services
+    //   const dynamodb = new AWS.DynamoDB({ credentials });
+
+    //   // Example: Query DynamoDB table
+    //   dynamodb.query({ TableName: 'YOUR_TABLE_NAME', KeyConditionExpression: 'PK = :pk' }, (err, data) => {
+    //     if (err) {
+    //       console.error('Error querying DynamoDB table:', err);
+    //       return;
+    //     }
+    //     console.log('DynamoDB query result:', data);
+    //   });
+    // });
+  }, []);
+
+
   // State to hold selected tabs
   // NEED TO FIX THIS TO NOT BE HARDCODED
   // const defaultTabs = {
