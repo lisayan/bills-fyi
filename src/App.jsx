78d1d1d00fa9cb6ce83a566bd16a1b88232509e5
Lisa Yan <lisayan@Lisas-MacBook-Pro.local>
2024-04-06 12:53:18 -0400

    localhost:3000/MRI to see MRI page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ChakraProvider, Box, Text } from "@chakra-ui/react";
 import Home from './pages/Home';
 import AddBill from './pages/AddBill';
+import MRI from './pages/MRI';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/addbillpage" element={<AddBill/>}/>
+        <Route path="/mri" element={<MRI/>}/>
       </Routes>
     </Router>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from "../components/Header";
 import DisplayData from "../components/DisplayData";
-import LiveBills from "../components/LiveBills"
+import ProcedurePanels from "../components/ProcedurePanels"
 
 export default function HomePage() {
     return (
         <div>
             <Header />
-            <LiveBills />
+            <ProcedurePanels />
             {/* <DisplayData /> */}
         </div>
     )
